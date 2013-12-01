c569566d164e18286ce48e94e2a2d7fe56ef33b3
alitari <alexander.krieg@arcor.de>
2013-12-01 15:41:17 +0100

    ready for unit testing

--- cards/core/src/test/java/de/alexkrieg/cards/core/CardTest.java
+++ cards/core/src/test/java/de/alexkrieg/cards/core/CardTest.java
@@ -6,15 +6,21 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+import playn.java.JavaPlatform;
+
 public class CardTest {
+
+	static {
+		JavaPlatform.register();
+	}
 
 	@Test
 	public void testInit() {
 		Card card = new Card(Card.Value._2c);
-		assertThat(card.layer(),nullValue());
+		assertThat(card.layer(), nullValue());
 		card.init();
-		//assertThat(card.layer(),notNullValue());
-		
+		assertThat(card.layer(), notNullValue());
+
 	}
 
 }
